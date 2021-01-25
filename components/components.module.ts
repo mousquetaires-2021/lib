ca38f70ac7f0c9643f5a458c1cb382e221c6e28f
François-Xavier Montigny <fxaviermontigny@gmail.com>
2021-01-25 08:32:56 +0100

    add form image

--- components/components.module.ts
+++ components/components.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from 'lib/libs/material.module';
 import { LoadingComponent } from './loading/loading.component';
+import { NgxImageCompressService } from 'ngx-image-compress';
 
 const list = [ LoadingComponent ];
 
 @NgModule({
 	declarations: [ ...list ],
 	imports: [ CommonModule, RouterModule, MaterialModule ],
+	providers: [ NgxImageCompressService ],
 	exports: list
 })
 export class ComponentsModule {}
