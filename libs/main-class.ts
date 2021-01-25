dca4189783e3bc5e91a25082f9e5a9f1344009dd
François-Xavier Montigny <fxaviermontigny@gmail.com>
2021-01-25 09:21:40 +0100

    add public method

--- libs/main-class.ts
+++ libs/main-class.ts
@@ -1,23 +1,35 @@
 import { Subscription } from 'rxjs';
 
 export class MainClass {
-  watcherList: Subscription[] = [];
+	watcherList: Subscription[] = [];
 
-  watch(sub) {
-    this.watcherList.push(sub);
-  }
+	watch(sub) {
+		this.watcherList.push(sub);
+	}
 
-  watcherDestroy() {
-    this.watcherList.map(w => {
-      try {
-        w.unsubscribe();
-      } catch (err) {}
-    });
-  }
+	watcherDestroy() {
+		this.watcherList.map((w) => {
+			try {
+				w.unsubscribe();
+			} catch (err) {}
+		});
+	}
 
-  trackBy(key) {
-    return function(index, value) {
-      return value[key];
-    };
-  }
+	trackBy(key) {
+		return function(index, value) {
+			return value[key];
+		};
+	}
+
+	public convertPriceToDollars(price) {
+		if (price > 30) {
+			return '€€€';
+		}
+
+		if (price > 10) {
+			return '€€';
+		}
+
+		return '€';
+	}
 }
