76b73616d5c3f346231e6db26373d1ab17c35746
François-Xavier Montigny <fxaviermontigny@gmail.com>
2021-01-26 09:10:59 +0100

    add order interface

--- interfaces/order-interface.ts
+++ interfaces/order-interface.ts
@@ -2,4 +2,11 @@ export interface OrderInferface {
 	id?: number;
 	restaurant_id?: number;
 	status?: number;
+	products?: ProductOrderInterface[];
+}
+
+export interface ProductOrderInterface {
+	id?: number;
+	product_id?: number;
+	quantity?: number;
 }
