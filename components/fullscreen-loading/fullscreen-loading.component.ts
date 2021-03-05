500b2bd2c86ae50fd30e51ecb51f240f5372515e
François-Xavier Montigny <fxaviermontigny@gmail.com>
2021-03-05 11:53:50 +0100

    add loader

--- components/fullscreen-loading/fullscreen-loading.component.ts
+++ components/fullscreen-loading/fullscreen-loading.component.ts
@@ -6,7 +6,29 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: [ './fullscreen-loading.component.scss' ]
 })
 export class FullscreenLoadingComponent implements OnInit {
-	constructor() {}
+	list = [
+		'/lib-assets/asian-food.svg',
+		'/lib-assets/cupcake-cherry.svg',
+		'/lib-assets/drink.svg',
+		'/lib-assets/french-fries.svg',
+		'/lib-assets/hot-dog.svg',
+		'/lib-assets/meat-steak.svg',
+		'/lib-assets/seafood-fish.svg'
+	];
+	index: number = 0;
+	constructor() {
+		setInterval(() => {
+			this.nextUpdate();
+		}, 750);
+	}
 
 	ngOnInit() {}
+
+	nextUpdate() {
+		if (this.list.length <= this.index + 1) {
+			this.index = 0;
+		} else {
+			this.index++;
+		}
+	}
 }
