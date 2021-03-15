f48874aea44181da34872b996159f2e5e42e6b19
François-Xavier Montigny <fxaviermontigny@gmail.com>
2021-03-15 17:22:58 +0100

    add performances

--- interfaces/performance-interface.ts
+++ interfaces/performance-interface.ts
@@ -1,6 +1,13 @@
+import { OrderInferface } from './order-interface';
+
 export interface PerformanceInferface {
 	date?: Date;
 	nbOrders?: number;
 	nbOrdersSuccessful?: number;
 	nbOrdersCancelled?: number;
+	totalWin?: number;
+	orders?: OrderInferface[];
+	totalBankFee?: number;
+	totalDelivreeticFee?: number;
+	totalDelivreeticFeeHT?: number;
 }
