24256123090355ada8f8b00a0f312abe5651f2c8
François-Xavier Montigny <fxaviermontigny@gmail.com>
2021-03-22 18:24:10 +0100

    add analytics

---
+++ services/analytics/analytics.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ServerService } from '../http-server/server.service';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class AnalyticsService {
+	constructor(private serverService: ServerService) {}
+
+	getGetMontlyCA(restaurantId) {
+		return this.serverService.get('analytics/get-preview-ca/' + restaurantId).then((data) => data.data || []);
+	}
+
+	getTopSelling(restaurantId) {
+		return this.serverService.get('analytics/top-selling/' + restaurantId).then((data) => data.data || []);
+	}
+}
