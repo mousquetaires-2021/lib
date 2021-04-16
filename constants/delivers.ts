fe49598034a5868ae06715b815635c8e42e016bc
François-Xavier Montigny <fxaviermontigny@gmail.com>
2021-04-16 09:49:46 +0200

    update status

--- constants/delivers.ts
+++ constants/delivers.ts
@@ -1,6 +1,6 @@
 export const DELIVER_STATUS = {
-	0: 'Profil à remplir',
-	1: 'En attente de validation',
+	0: 'Profil à compléter',
+	1: 'En validation par Delivreetic',
 	2: 'Prêt à livrer'
 };
 
@@ -10,35 +10,35 @@ export const DELIVER_STATUS_LABEL = Object.entries(DELIVER_STATUS).map((key, val
 }));
 
 export const DELIVER_WAY = {
-	0: 'Vélo',
-	1: 'Vélo cargo',
-	2: 'Vélo électrique',
-	3: 'Scooter',
-	4: 'Scooter électrique',
-	5: 'Voiture',
-	6: 'Voiture électrique',
-	8: 'Je loue',
-	7: 'Autre'
+	bike: 'Vélo',
+	'cargo-bike': 'Vélo cargo',
+	'electric-bike': 'Vélo électrique',
+	scooter: 'Scooter',
+	'electric-scooter': 'Scooter électrique',
+	car: 'Voiture',
+	'electric-car': 'Voiture électrique',
+	rent: 'Je loue',
+	other: 'Autre'
 };
 
 export const DELIVER_WAY_LABEL = Object.entries(DELIVER_WAY).map((key, value) => ({
-	id: value,
-	label: DELIVER_WAY[value]
+	id: key[0],
+	label: key[1]
 }));
 
 export const DELIVER_DEPARTEMENT = {
-	95: "Val-d'Oise",
-	78: 'Yvelines',
-	91: 'Essonne',
-	77: 'Seine-et-Marne',
-	92: 'Hauts-de-Seine',
-	75: 'Paris',
-	93: 'Seine-St-Denis',
-	94: 'Val-de-Marne',
-	0: 'Autre'
+	'95': "Val-d'Oise",
+	'78': 'Yvelines',
+	'91': 'Essonne',
+	'77': 'Seine-et-Marne',
+	'92': 'Hauts-de-Seine',
+	'75': 'Paris',
+	'93': 'Seine-St-Denis',
+	'94': 'Val-de-Marne',
+	'0': 'Autre'
 };
 
 export const DELIVER_DEPARTEMENT_LABEL = Object.entries(DELIVER_DEPARTEMENT).map((key, value) => ({
-	id: +key[0],
+	id: key[0],
 	label: key[1]
 }));
