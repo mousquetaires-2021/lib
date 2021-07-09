21a81abcfa3e59bf2cae79fedb6feb6610844a3f
François-Xavier Montigny <francois-xavier@delivreetic.com>
2021-07-09 14:11:07 +0200

    add redirection after connect

--- services/app/app.service.ts
+++ services/app/app.service.ts
@@ -1,15 +1,30 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: "root",
 })
 export class AppService {
-	loading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-	needAccountToView: BehaviorSubject<string> = new BehaviorSubject<string>(null);
-	constructor() {}
+  loading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  needAccountToView: BehaviorSubject<string> = new BehaviorSubject<string>(
+    null
+  );
+  constructor(private authService: AuthService) {}
 
-	needAccount(message) {
-		this.needAccountToView.next(message);
+  needAccount(message, cleanUrl = false) {
+    if (message) {
+      this.authService.redirectUrl =
+        window.location.pathname + window.location.search;
+    }
+
+	if(!message && cleanUrl) {
+		this.authService.redirectUrl = null
 	}
+	
+	console.log("redirectUrl", this.authService.redirectUrl);
+
+
+    this.needAccountToView.next(message);
+  }
 }
