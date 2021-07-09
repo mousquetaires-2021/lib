3f15de5334195da52a907c903b913ace8e0a729f
François-Xavier Montigny <francois-xavier@delivreetic.com>
2021-07-09 09:13:04 +0200

    add delay selling

--- services/analytics/analytics.service.ts
+++ services/analytics/analytics.service.ts
@@ -1,17 +1,21 @@
-import { Injectable } from '@angular/core';
-import { ServerService } from '../http-server/server.service';
+import { Injectable } from '@angular/core'
+import { ServerService } from '../http-server/server.service'
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnalyticsService {
-	constructor(private serverService: ServerService) {}
+  constructor (private serverService: ServerService) {}
 
-	getGetMontlyCA(restaurantId) {
-		return this.serverService.get('analytics/get-preview-ca/' + restaurantId).then((data) => data.data || []);
-	}
+  getGetMontlyCA (restaurantId) {
+    return this.serverService.get('analytics/get-preview-ca/' + restaurantId).then((data) => data.data || [])
+  }
 
-	getTopSelling(restaurantId) {
-		return this.serverService.get('analytics/top-selling/' + restaurantId).then((data) => data.data || []);
-	}
+  getTopSelling (restaurantId) {
+    return this.serverService.get('analytics/top-selling/' + restaurantId).then((data) => data.data || [])
+  }
+
+  getDailySelling () {
+    return this.serverService.get('analytics/get-daily-selling').then((data) => data.data || [])
+  }
 }
