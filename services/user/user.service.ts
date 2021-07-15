8fd0102e1d9b238018465d74c4c256af4c95863c
François-Xavier Montigny <francois-xavier@delivreetic.com>
2021-07-15 09:51:25 +0200

    add user customer

--- services/user/user.service.ts
+++ services/user/user.service.ts
@@ -1,166 +1,174 @@
-import { Injectable } from "@angular/core";
-import { UserInterface } from "lib/interfaces/user-interface";
-import { BehaviorSubject } from "rxjs";
-import { environment } from "src/environments/environment";
-import { ServerService } from "../http-server/server.service";
-import { RestaurantService } from "../restaurant/restaurant.service";
+import { Injectable } from '@angular/core'
+import { UserInterface } from 'lib/interfaces/user-interface'
+import { BehaviorSubject } from 'rxjs'
+import { environment } from 'src/environments/environment'
+import { ServerService } from '../http-server/server.service'
+import { RestaurantService } from '../restaurant/restaurant.service'
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class UserService {
   token: BehaviorSubject<string> = new BehaviorSubject<string>(null);
-  user: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  user: BehaviorSubject<UserInterface> = new BehaviorSubject<UserInterface>(null);
   position: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
-  constructor(
+  constructor (
     private serverService: ServerService,
     private restaurantService: RestaurantService
   ) {
     this.token.subscribe((token) => {
-      console.log("TOKEN", token);
-      this.updateNotificationsToken();
-    });
+      console.log('TOKEN', token)
+      this.updateNotificationsToken()
+    })
     this.position.subscribe(() => {
-      this.updateNotificationsToken();
-    });
+      this.updateNotificationsToken()
+    })
   }
 
-  setUser(user) {
-    this.user.next(user);
+  setUser (user) {
+    this.user.next(user)
 
     if (user && user.token) {
-      this.serverService.setToken(user.token);
+      this.serverService.setToken(user.token)
     }
   }
 
-  me() {
-    return this.serverService.get("users/me").then((data) => data.data || null);
+  me () {
+    return this.serverService.get('users/me').then((data) => data.data || null)
   }
 
-  updateMe(params) {
+  updateMe (params) {
     return this.serverService
-      .put("users/update-me", params)
-      .then((data) => data.data || null);
+      .put('users/update-me', params)
+      .then((data) => data.data || null)
   }
 
-  updatePasswordMe(params) {
+  updatePasswordMe (params) {
     return this.serverService
-      .put("users/update-password-me", params)
-      .then((data) => data.data || null);
+      .put('users/update-password-me', params)
+      .then((data) => data.data || null)
   }
 
-  updateAccount(params) {
+  updateAccount (params) {
     return this.serverService
-      .put("users/update-account", params)
+      .put('users/update-account', params)
       .then((data) => {
-        return data;
-      });
+        return data
+      })
   }
 
-  createAccount(params: UserInterface) {
+  createAccount (params: UserInterface, saveToken = true) {
     return this.serverService
-      .post("users/create-account", params)
+      .post('users/create-account', params)
       .then((data) => {
-        this.serverService.setToken(data.token);
-        return true;
-      });
+        if(saveToken) {
+          this.serverService.setToken(data.token)
+        }
+        return true
+      })
   }
 
-  getUsers() {
-    return this.serverService.get("users/list").then((data) => data.data || []);
+  getUsers () {
+    return this.serverService.get('users/list').then((data) => data.data || [])
   }
 
-  getUser(id) {
+  getCustomers () {
     return this.serverService
-      .get("users/get/" + id)
-      .then((data) => data.data || null);
+      .get('users/list-customers-only')
+      .then((data) => data.data || [])
   }
 
-  mobileCreation(phone) {
+  getUser (id) {
     return this.serverService
-      .post("users/mobile-creation", { phone })
-      .then((data) => data.data || null);
+      .get('users/get/' + id)
+      .then((data) => data.data || null)
   }
 
-  mobileValidation(phone, code) {
+  mobileCreation (phone) {
     return this.serverService
-      .post("users/mobile-validation", { phone, code })
-      .then((data) => data.data || null);
+      .post('users/mobile-creation', { phone })
+      .then((data) => data.data || null)
   }
 
-  accountForgotPassword(contact) {
+  mobileValidation (phone, code) {
     return this.serverService
-      .put("users/forgot-password", { contact })
-      .then((data) => data.data || null);
+      .post('users/mobile-validation', { phone, code })
+      .then((data) => data.data || null)
   }
 
-  lightForgotPassword(params) {
+  accountForgotPassword (contact) {
     return this.serverService
-      .put("users/light-forgot-password", params)
-      .then((data) => data.data || null);
+      .put('users/forgot-password', { contact })
+      .then((data) => data.data || null)
   }
 
-  logout() {
-    return this.serverService.get("auths/logout").then(() => {
-      this.restaurantService.restaurants.next([]);
-      this.restaurantService.restaurant.next(null);
-      this.user.next(null);
-      this.serverService.removeToken();
-    });
-  }
-
-  userDetails() {
+  lightForgotPassword (params) {
     return this.serverService
-      .get("users/account-details")
-      .then((data) => data.data || null);
+      .put('users/light-forgot-password', params)
+      .then((data) => data.data || null)
   }
 
-  updateUserDetails(params) {
-    return this.serverService.put("users/update-account-details", params);
+  logout () {
+    return this.serverService.get('auths/logout').then(() => {
+      this.restaurantService.restaurants.next([])
+      this.restaurantService.restaurant.next(null)
+      this.user.next(null)
+      this.serverService.removeToken()
+    })
   }
 
-  updateNotificationsToken() {
-    const token = this.token.getValue();
+  userDetails () {
+    return this.serverService
+      .get('users/account-details')
+      .then((data) => data.data || null)
+  }
+
+  updateUserDetails (params) {
+    return this.serverService.put('users/update-account-details', params)
+  }
+
+  updateNotificationsToken () {
+    const token = this.token.getValue()
     if (!token) {
-      return;
+      return
     }
-    const position = this.position.getValue();
-    const latitude = position && position.latitude ? position.latitude : null;
+    const position = this.position.getValue()
+    const latitude = position && position.latitude ? position.latitude : null
     const longitude =
-      position && position.longitude ? position.longitude : null;
+      position && position.longitude ? position.longitude : null
 
-    return this.serverService.postWithoutError("notifications/update", {
+    return this.serverService.postWithoutError('notifications/update', {
       token,
       type: environment.tokenType,
       latitude,
       longitude,
-    });
+    })
   }
 
-  updateNotificationsTokenEnable(status) {
-    const token = this.token.getValue();
+  updateNotificationsTokenEnable (status) {
+    const token = this.token.getValue()
     if (!token) {
-      return;
+      return
     }
 
-    return this.serverService.postWithoutError("notifications/update-enable", {
+    return this.serverService.postWithoutError('notifications/update-enable', {
       token,
-      notifiable: status ? 1 : 0
-    });
+      notifiable: status ? 1 : 0,
+    })
   }
 
-  getNoficiationStatus() {
-    return this.serverService.get(`notifications/notification-status/${this.token.getValue()}`).then((data) => data.data);
+  getNoficiationStatus () {
+    return this.serverService.get(`notifications/notification-status/${this.token.getValue()}`).then((data) => data.data)
   }
 
-  removeNotificationsToken(token) {
-    return this.serverService.post("notifications/remove", { token });
+  removeNotificationsToken (token) {
+    return this.serverService.post('notifications/remove', { token })
   }
 
-  addForcePosition(alert = true) {
+  addForcePosition (alert = true) {
     return new Promise((resolve, reject) => {
-      reject("No device");
-    });
+      reject('No device')
+    })
   }
 }
