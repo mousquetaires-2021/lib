82b159977b93089d700af9a05c33ce53f20b90e5
François-Xavier Montigny <francois-xavier@delivreetic.com>
2021-07-30 16:40:35 +0200

    add popup with connection text

--- services/app/app.service.ts
+++ services/app/app.service.ts
@@ -18,9 +18,9 @@ export class AppService {
         window.location.pathname + window.location.search;
     }
 
-	if(!message && cleanUrl) {
-		this.authService.redirectUrl = null
-	}
+    if (!message && cleanUrl) {
+      this.authService.redirectUrl = null;
+    }
 
     this.needAccountToView.next(message);
   }
