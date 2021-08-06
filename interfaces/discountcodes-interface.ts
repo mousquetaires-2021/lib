c84922c5952d017e9c950049e2c54c7fe6aef07f
François-Xavier Montigny <francois-xavier@delivreetic.com>
2021-08-06 15:59:01 +0200

    add discount code

---
+++ interfaces/discountcodes-interface.ts
@@ -0,0 +1,15 @@
+import { RestaurantInferface } from './restaurant-interface'
+
+export interface DiscountcodesInterface {
+  id?: number;
+  name?: string;
+  code?: string;
+  reduce_percent?: number;
+  reduce_value?: number;
+  nb_max_use?: number;
+  nb_use?: number;
+  minimum_total?: number;
+  enable?: boolean;
+  date_limit?: Date;
+  restaurant?: RestaurantInferface;
+}
