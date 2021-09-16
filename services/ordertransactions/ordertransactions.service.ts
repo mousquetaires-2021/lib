badd0a3dfba192c9320d364ed61456d01230907d
François-Xavier Montigny <francois-xavier@delivreetic.com>
2021-09-16 09:56:18 +0200

    change transaction

--- services/ordertransactions/ordertransactions.service.ts
+++ services/ordertransactions/ordertransactions.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@angular/core'
-import { ServerService } from '../http-server/server.service'
+import { Injectable } from "@angular/core";
+import { ServerService } from "../http-server/server.service";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class OrdertransactionsService {
-	constructor (private serverService: ServerService) { }
+  constructor(private serverService: ServerService) {}
 
-	getOrderStatus (orderId: number) {
-	  return this.serverService
-	    .post('ordertransactions/get-order-status', { orderId })
-	    .then((data) => data.data || null)
-	}
+  getOrderStatus(orderId: number) {
+    return this.serverService
+      .post("ordertransactions/get-order-status", { orderId })
+      .then((data) => data.data || null);
+  }
+
+  cancelTransaction(transactionId: number) {
+    return this.serverService.post("ordertransactions/remove-transaction", {
+      transactionId,
+    });
+  }
 }
